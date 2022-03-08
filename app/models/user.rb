0b406c4ab37dcfb0c542d766a52406cc70be86d9
ryoya horiuchi <appleorange0720@gmail.com>
2022-03-08 15:44:03 +0900

    フォローシステム作成

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, uniqueness: true
+
+  before_validation { email.downcase! }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def follow!(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
+  def unfollow!(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
+  resources :users, only: %i[ new create show index ]
+  resources :sessions, only: %i[ new create destroy ]
+  resources :relationships, only: %i[ create destroy ]
+end
